1b6d07f2eb475f73b3e6f8b7423acc4b0326dcc5
Chirag Rangras <chiragrangras012@gmail.com>
2025-02-10 16:47:45 +0530

    new file added

---
+++ Javascript_fundamentals-part_1/script.js
@@ -0,0 +1,117 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Joanas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let joanas_matilda = "JM";
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(null);
+
+let age = 30;
+age = 32;
+
+const birthYear = 1990;
+// birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+LastName = 'Schmedtmann';
+console.log(LastName);
+
+// Math operators
+const now = 2025;
+const ageJonas = now - 1993;
+const ageSarah = now - 1999;
+console.log(ageJonas, ageSarah);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assigment operators
+let x = 10 + 5; // 15
+x += 10; // x= x+10 =>25;
+console.log(x);
+
+// Comparsion operators
+console.log(ageJonas > ageSarah); // >,<,>=,<=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1993 > now - 2025);
+
+const now = 2025;
+const ageJonas = now - 1993;
+const ageSarah = now - 1999;
+
+console.log(now - 1993 > now - 2025);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+const firstName = "Jonas";
+const job = "techer";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
