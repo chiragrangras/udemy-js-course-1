2edab00b26ffc902981134c90d09ce28f8589c3c
Chirag Rangras <chiragrangras012@gmail.com>
2025-02-20 15:35:20 +0530

    new script added

--- Javascript_fundamentals-part_1/script.js
+++ Javascript_fundamentals-part_1/script.js
@@ -203,3 +203,24 @@ if (favourite === 23) {
 if (favourite !== 23) console.log("Why not 237");
 */
 
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sara is able to drive!");
+// } else {
+//   console.log("Someone else should drive....");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sara is able to drive!");
+} else {
+  console.log("Someone else should drive....");
+}
