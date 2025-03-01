11bb8eb2f3410abf6515454e5a37dab9e6850428
Chirag Rangras <chiragrangras012@gmail.com>
2025-03-01 17:42:35 +0530

    new script added

--- Javascript_fundamentals-part_1/script.js
+++ Javascript_fundamentals-part_1/script.js
@@ -268,3 +268,11 @@ if(day === 'monday') {
     console.log("Not a valid day!");
 }
 */
+
+const age = 23;
+age >= 18 ? console.log("I like to drink wine 🍷") : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" :  "water 💧";
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
