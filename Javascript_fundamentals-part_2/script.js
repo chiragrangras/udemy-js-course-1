1de814f1856dfd7ecde924f4b39ccf2551e497a6
Chirag Rangras <chiragrangras012@gmail.com>
2025-03-03 12:36:05 +0530

    new script added

--- Javascript_fundamentals-part_2/script.js
+++ Javascript_fundamentals-part_2/script.js
@@ -8,7 +8,7 @@ if(hasDriversLicense) console.log('I can drive 🚗🚗🚗🚗');
 
 // const interface = 'Audio';
 // const private = 534;
-// const if = 23; */
+// const if = 23; 
 
 function logger() {
     console.log('My name is Jonas');
@@ -27,3 +27,35 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+// Function Declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+
+// Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
