2572d6099992b6a56d1aaf8bcbc7e33eb29e9363
Chirag Rangras <chiragrangras012@gmail.com>
2025-04-10 17:17:02 +0530

    modified file

--- Javascript_fundamentals-part_2/script.js
+++ Javascript_fundamentals-part_2/script.js
@@ -200,13 +200,18 @@ console.log(jonas.age);
 console.log(jonas.getSummary());
 */
 
-console.log('Lifting weights repetition 1 🏋️‍♂️');
-console.log('Lifting weights repetition 2 🏋️‍♂️');
-console.log('Lifting weights repetition 3 🏋️‍♂️');
-console.log('Lifting weights repetition 4 🏋️‍♂️');
-console.log('Lifting weights repetition 5 🏋️‍♂️');
-console.log('Lifting weights repetition 6 🏋️‍♂️');
-console.log('Lifting weights repetition 7 🏋️‍♂️');
-console.log('Lifting weights repetition 8 🏋️‍♂️');
-console.log('Lifting weights repetition 9 🏋️‍♂️');
-console.log('Lifting weights repetition 10 🏋️‍♂️');
+// console.log('Lifting weights repetition 1 🏋️‍♂️');
+// console.log('Lifting weights repetition 2 🏋️‍♂️');
+// console.log('Lifting weights repetition 3 🏋️‍♂️');
+// console.log('Lifting weights repetition 4 🏋️‍♂️');
+// console.log('Lifting weights repetition 5 🏋️‍♂️');
+// console.log('Lifting weights repetition 6 🏋️‍♂️');
+// console.log('Lifting weights repetition 7 🏋️‍♂️');
+// console.log('Lifting weights repetition 8 🏋️‍♂️');
+// console.log('Lifting weights repetition 9 🏋️‍♂️');
+// console.log('Lifting weights repetition 10 🏋️‍♂️');
+
+// for loop keeps running while condition is TRUE
+for(let rep = 1; rep <=5; rep++){
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+}
