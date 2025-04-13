c5521d8983820528be9c367de744223241ed4b5d
Chirag Rangras <chiragrangras012@gmail.com>
2025-04-13 11:31:59 +0530

    modified file

--- Javascript_fundamentals-part_2/script.js
+++ Javascript_fundamentals-part_2/script.js
@@ -278,21 +278,20 @@ for (let exercise = 1; exercise < 4; exercise++) {
 // for (let rep = 1; rep <= 10; rep++) {
     //     console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
     // }
-
-// console.log("--- WHILE LOOP ---");
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
-  rep++;
-}
+    
+    // console.log("--- WHILE LOOP ---");
+    let rep = 1;
+    while (rep <= 10) {
+        // console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+        rep++;
+    }
+    */
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 console.log(dice);
 
 while (dice !== 6) {
-    console.log(`You rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) console.log("Loop is about to end...");
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
-*/
-
