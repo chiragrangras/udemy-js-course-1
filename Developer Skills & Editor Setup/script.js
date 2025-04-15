eb5cbb0c51a1818614e8dfd165f6c0ae539715c2
Chirag Rangras <chiragrangras012@gmail.com>
2025-04-15 17:50:04 +0530

    modified file

--- Developer Skills & Editor Setup/script.js
+++ Developer Skills & Editor Setup/script.js
@@ -1,3 +1,12 @@
 "use stict";
 
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calsTempAmlitude = function (temps) {
+  let max = temps[0];
+  for (i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
+calsTempAmlitude([3,7,4]);
