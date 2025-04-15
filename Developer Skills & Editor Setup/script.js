50af6a29da72559f94d49c81c63b5dabe9dc4eb7
Chirag Rangras <chiragrangras012@gmail.com>
2025-04-15 18:32:51 +0530

    modified file

--- Developer Skills & Editor Setup/script.js
+++ Developer Skills & Editor Setup/script.js
@@ -4,9 +4,22 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 const calsTempAmlitude = function (temps) {
   let max = temps[0];
-  for (i = 0; i < temps.length; i++) {
-    if (temps[i] > max) max = temps[i];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
   }
-  console.log(max);
+  console.log(max, min);
+  return max - min;
 };
-calsTempAmlitude([3,7,4]);
+const amplitude = calsTempAmlitude(temperatures);
+console.log(amplitude);
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = array1.concat(array2);
+console.log(array3); // [1, 2, 3, 4, 5, 6]
