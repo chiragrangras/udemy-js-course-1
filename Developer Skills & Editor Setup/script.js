a6c8031d84072fa56e6c87e98bd3eb5cff95b93a
Chirag Rangras <chiragrangras012@gmail.com>
2025-04-19 17:47:43 +0530

    modifid file

--- Developer Skills & Editor Setup/script.js
+++ Developer Skills & Editor Setup/script.js
@@ -29,7 +29,7 @@ const measureKelvin = function () {
   const measurement = {
     type: "temp",
     unit: "celsius",
-    
+
     // C) FIX
     value: Number(prompt("Degrees celsius:")),
   };
@@ -43,7 +43,6 @@ const measureKelvin = function () {
 
 // A) IDENTIFY
 console.log(measureKelvin());
-*/
 
 // 1) Understanding the problem
 // - Array transformed to string, separated by ... (comma, space, etc.)
@@ -67,4 +66,45 @@ const printforcasr = function (arr) {
   }
   console.log('... '+ str);
 };
+
 printforcasr(data1);
+*/
+function analysisWorkWeek(dailyHours) {
+  const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  if (!Array.isArray(dailyHours) || dailyHours.length !== 7) {
+    throw new Error("Input must be an array of 7 daily hours.");
+  }
+
+  const totalHours = dailyHours.reduce((sum, hours) => sum + hours, 0);
+  const averageHours = +(totalHours / dailyHours.length).toFixed(1);
+  const maxhours = Math.max(...dailyHours);
+  const maxDayIndex = dailyHours.indexOf(maxhours);
+  const maxDays = daysOfWeek[maxDayIndex];
+  const daysWorked = dailyHours.filter((hours) => hours > 0).length;
+  const isFullTime = totalHours >= 35;
+
+  return {
+    totalHours,
+    averageHours,
+    maxDays,
+    daysWorked,
+    isFullTime,
+  };
+}
+
+const weeklyHours = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+const analysis = analysisWorkWeek(weeklyHours);
+console.log(analysis);
+
+const weeklyHours2 = [7.5, 8, 6.5, 0, 8.5];
+const analysis2 = analysisWorkWeek(weeklyHours2);
+console.log(analysis2);
