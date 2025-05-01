96d8b67a0a9badf735cc9b34d70cb4e47db21075
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-01 18:00:34 +0530

    new file

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,3 +1,27 @@
 "use stirct";
 
-const score0 = document.querySelector("#score--0");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generate a radom dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display the dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  
+});
