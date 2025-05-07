6858303a3f01d2fa09e975379c3e201eda48e669
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-07 16:54:09 +0530

    new file added

---
+++ 08-Behind-the-Scenes/script.js
@@ -0,0 +1,27 @@
+"use stickt";
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      
+      var millenial = true;  
+      const str = `Oh, and you're a millennial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenial);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = "Jonas";
+calcAge(1991);
+// console.log(age);
+// printAge();
