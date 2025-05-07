d9db9a931aca6e8889636ac01c8af91bea643be2
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-07 17:31:12 +0530

    new file added

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,20 +1,30 @@
-"use stickt";
+"use strict";
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   console.log(firstName);
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Create a new variable with same name as outer scope's variable
+      const firstName = "Steven";
       
-      var millenial = true;  
+      // Reassigning the output variable declared in the outer scope
+      output = "NEW OUTPUT!";
       const str = `Oh, and you're a millennial, ${firstName}`;
       console.log(str);
+
+      function add(a,b){
+        return a + b;
+      }
     }
     // console.log(str);
     console.log(millenial);
+    // console.log(add(2,3));
+    console.log(output);
   }
   printAge();
 
