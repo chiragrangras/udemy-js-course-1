ed0b43f026801a5064a3c7d7ef0d5fcc78382236
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-09 11:30:16 +0530

    new file added

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -27,7 +27,6 @@
     console.log(output);
   }
   printAge();
-
   return age;
 }
 
@@ -42,11 +41,40 @@ console.log(me);
 // console.log(job);
 // console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
+var me = "Jonas";
+let job = "teacher";
 const year = 1991;
 
 // Functions
-function addDecl(a,b){
-    return a + b;
+console.log(addDecl(2, 3)); // Hoisted
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+// console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
