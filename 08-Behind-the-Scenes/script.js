449bc20f42ada8f6ddfa28d3e6b7137158e3de81
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-13 18:17:42 +0530

    modified file

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -110,7 +110,6 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
-*/
 
 const jonas = {
   firstName: "joanas",
@@ -119,12 +118,22 @@ const jonas = {
     // console.log(this);
     console.log(2037 - this.year);
 
-    const isMillenial = function () {
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
       console.log(this);
       console.log(this.year >= 1981 && this.year <= 1996);
     };
+
     isMillenial();
   },
+
   greet: () => {
     console.log(this);
     console.log(`Hey ${this.firstName}`);
@@ -132,3 +141,40 @@ const jonas = {
 };
 jonas.greet();
 jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+*/
+
+////////////////////////////////////////
+// Objects Reference in Primitive (Shallow Vs. Deep Copies)
+
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+};
+
+function marryPerson(originalPerson, newLastName) {
+  originalPerson.lastName = newLastName;
+  return originalPerson;
+}
+
+const marriedJessica = marryPerson(jessica, "Davis");
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = "Davis";
+
+console.log("Before:", jessica);
+console.log("After:", marriedJessica);
