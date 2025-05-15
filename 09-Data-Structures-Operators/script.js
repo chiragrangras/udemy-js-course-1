a2c3d379686a036571eccc4028b50f0ed91cf04e
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-15 11:02:35 +0530

    new file

---
+++ 09-Data-Structures-Operators/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const restaurtant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Capresse Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
