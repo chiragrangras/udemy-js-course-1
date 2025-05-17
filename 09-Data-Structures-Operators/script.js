ee766740cd155807c0eb523ca9964adeaad14aa9
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-17 12:03:03 +0530

    new file added

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -6,34 +6,69 @@ const restaurtant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Capresse Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours:{
-    thu:{
-      open:12,
-      close:22,
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-    fri:{
-      open:11,
-      close:23,
+    fri: {
+      open: 11,
+      close: 23,
     },
-    sat:{
-      open:0, // Open 24 hours
-      close:24,
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
     },
   },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(`order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
 };
 
-const {name, openingHours, categories} = restaurtant;
+restaurtant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurtant.orderDelivery({
+  address: "Via del Sole, 21",
+  starterIndex: 1,
+})
+
+const { name, openingHours, categories } = restaurtant;
 console.log(name, openingHours, categories);
 
-const {name: restaurantName, openingHours: hours, categories: tags} = restaurtant;
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurtant;
 console.log(restaurantName, hours, tags);
 
-const {menu = [], starterMenu: starters = []} = restaurtant;
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurtant;
 console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
 
 /*
 const arr = [2, 3, 4];
