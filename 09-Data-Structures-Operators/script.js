c678905b4ca9725f8f2b6af1803fa75fa69b7a76
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-24 18:18:55 +0530

    new file added

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,9 +49,76 @@ const restaurtant = {
   },
 };
 
+const questions = new Map([
+  ["question", "What is the best programming language in the world ?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again!👎"],
+]);
+console.log(questions);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(questions.get("question"));
+for (const [key, value] of questions) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+
+console.log(questions.get(questions.get("correct") === answer));
+
+// Convert map to array
+console.log(...questions);
+// console.log(...questions.entries());
+console.log([...questions.keys()]);
+console.log([...questions.values()]);
+
+/*
+///////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time >= rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
 //////////////////////////////////////
 // New Operations to Make Sets Useful!
-
 const ItalianFoods = new Set([
   "pasta",
   "gnocchi",
@@ -76,7 +143,19 @@ console.log("Intersection:", commonFoods);
 const ItalianMexicanFusion = ItalianFoods.union(mexicanFoods);
 console.log("Union:", ItalianMexicanFusion);
 
-/*
+console.log([...new Set([...ItalianFoods, ...mexicanFoods])]);
+
+const uniqueItalianFoods = ItalianFoods.difference(mexicanFoods);
+console.log("Difference italian:", uniqueItalianFoods);
+
+const uniqueMexicanFoods = mexicanFoods.difference(ItalianFoods); 
+console.log("Difference mexican:", uniqueMexicanFoods);
+
+const uniqueItalianFoodsAndMexicanFoods = ItalianFoods.symmetricDifference(mexicanFoods);
+console.log("Symmetric Difference:", uniqueItalianFoodsAndMexicanFoods);
+
+console.log(ItalianFoods.isDisjointFrom(mexicanFoods));
+
 ///////////////////////////////////////
 // Sets
 const orderSet = new Set([
