ac92bc30690e432fdc4b09dd6ded26aca2713a69
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-28 16:38:54 +0530

    new file added

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,56 @@ const restaurtant = {
   },
 };
 
+// Split and Join Methods
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
+
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const captialization = function(name){
+  const names = name.split(" ");
+  const namesUpper = [];
+
+  for(const n of names){
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+}
+
+captialization("jessica ann smith davis");
+captialization("jonas schmedtmann");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+").padEnd(30, "+"));
+
+const maskCreditCard = function(number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+}
+
+console.log(maskCreditCard(12345678));
+console.log(maskCreditCard(12345678901234567));
+console.log(maskCreditCard("1234 5678 9012 3456"));
+
+// Repeat
+const message2 = "Bad weather... All Departures Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function(n) {
+  console.log(`There are ${n} planes in line ${"✈️".repeat(n)}`);
+}
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+////////////////////////////////////
+// Working with Strings - Part 2
 const airlines = "TAP Air Portugal";
 
 console.log(airlines.toLowerCase());
