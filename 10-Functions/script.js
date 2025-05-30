11ed5df9c3aab3a5a4fc72f4541334ec41252b26
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-30 18:57:35 +0530

    new file added

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -87,11 +87,17 @@ const checkIn = function (flightNum, passenger) {
   }
 }
 
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
 // Is the same as doing...
-const flightNum = flight;
-const passenger = jonas;
+// const flightNum = flight;
+// const passenger = jonas;
 
+const newPassport = function(person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+}
+
+newPassport(jonas);
+checkIn(flightNum, jonas);
