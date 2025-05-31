7cb733c471f4fd3fe17fa5ac99f9c480bcff6c2e
Chirag Rangras <chiragrangras012@gmail.com>
2025-05-31 17:43:54 +0530

    new file added

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -68,7 +68,6 @@ bookings.push(booking1, booking2, booking3, booking4, booking5);
 bookings.forEach((booking) => {
   console.log(JSON.stringify(booking.getBookingSummary()));
 });
-*/
 
 const flight = "LH234";
 const jonas = {
@@ -101,3 +100,31 @@ const newPassport = function(person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
