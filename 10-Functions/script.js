2263405252b44df93cfd2e3723d924ce6ef246c7
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-02 18:06:40 +0530

    new file added

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -159,7 +159,7 @@ const lufthansa = {
 
 lufthansa.book(239,'Jonas Schmedtmann');
 lufthansa.book(635, 'John Smith');
-console.log(lufthansa);
+
 
 const eurowings = {
   name: 'Eurowings',
@@ -172,5 +172,23 @@ const book = lufthansa.book;
 // Does NOT work
 // book(23, 'Sarah Williams');
 
+// Call method
 book.call(eurowings, 23, 'Sarah Williams');
 console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+}
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
