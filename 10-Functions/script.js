67ae26f75f6d12e42b4b06f6b1d7f563b3829b98
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-04 18:29:17 +0530

    new file added

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -298,4 +298,16 @@ runOnce();
 console.log(notPrivate); // 46
 */
 
+const secureBooking = function () {
+  let passengerCount = 0;
 
+  return function () {
+    passengerCount++;
+    console.log(`Passenger count: ${passengerCount}`);
+  };
+}
+
+const booker = secureBooking();
+booker(); // Passenger count: 1 
+booker(); // Passenger count: 2
+booker(); // Passenger count: 3
