a73616302adc1d4cbb7e17b9bdbe7ee702ff681d
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-07 17:19:11 +0530

    new file added

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -75,6 +75,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE METHOD
@@ -90,3 +91,29 @@ console.log([...arr]);
 // console.log(arr.splice(2));
 arr.splice(-1);
 console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE METHOD
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT METHOD
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN METHOD
+console.log(letters.join(' - '));
+*/
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting the last element of an array
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
