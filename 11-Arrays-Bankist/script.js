cc260f06dd0efd82a8457d4794ff99257cbb15c7
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-07 18:22:02 +0530

    new file added

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -109,3 +109,16 @@ console.log([...arr, ...arr2]);
 console.log(letters.join(' - '));
 */
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for(const movement of movements){
+  if(movement > 0){
+    console.log(`You deposited ${movement}`);
+  }else{
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function(){
+  
+})
