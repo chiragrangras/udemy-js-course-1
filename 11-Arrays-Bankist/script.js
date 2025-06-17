202ffcf4392594db5f5a210108a677c7d9febe71
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-17 15:08:54 +0530

    new file added

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -86,29 +86,29 @@ const calcDisplayBalance = function (movements) {
 };
 calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes} €`;
 
-  const out = movements
+  const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} €`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter((mov) => mov > 0)
     //.map((deposit) => (deposit * account1.interestRate) / 100);
     .map((deposit) => (deposit * 1.2) / 100)
-    .filter((int,i,arr)=>{
-      console.log(arr);
+    .filter((int, i, arr) => {
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest} €`;
 };
-calcDisplaySummary(account1.movements);
+
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -120,6 +120,42 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
+
+// Event handlers
+
+let currentAccount;
+
+btnLogin.addEventListener("click", function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+
+    labelWelcome.textContent = `Welcome back ,${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 1;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    // Display movements
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -279,18 +315,27 @@ const max = movements.reduce((acc, mov) => {
   else return mov;
 },movements[0]);
 console.log(max);
-*/
 
 const euroToUsd = 1.1;
 console.log(movements);
 
 // PIPELINE
 const totalDepositsUSD = movements
-  .filter((mov) => mov > 0)
-  .map((mov, i, arr) => {
-    // console.log(arr);
-    return mov * euroToUsd;
-  })
-  // .map((mov) => mov * euroToUsd)
-  .reduce((acc, mov) => acc + mov, 0);
+.filter((mov) => mov > 0)
+.map((mov, i, arr) => {
+  // console.log(arr);
+  return mov * euroToUsd;
+})
+// .map((mov) => mov * euroToUsd)
+.reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+const fistWithdrawal = movements.find((mov) => mov < 0);
+console.log(movements);
+console.log(fistWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account);
+*/
