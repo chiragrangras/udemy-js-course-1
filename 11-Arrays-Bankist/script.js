950b3c5786b511c0f8665c420122b0b4189fb0ad
Chirag Rangras <chiragrangras012@gmail.com>
2025-06-19 16:55:35 +0530

    new file added

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -181,6 +181,24 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -409,8 +427,18 @@ console.log(movements);
 // EQUALITY
 console.log(movements.includes(-130));
 
-// CONDITION
-console.log(movements.some(mov => mov === -130));
+// SOME : CONDITION
+console.log(movements.some((mov) => mov === -130));
 
 const anyDeposits = movements.some((mov) => mov > 1500);
 console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every((mov) => mov > 0));
+console.log(account4.movements.every((mov) => mov > 0));
+
+// SEPARATE CALLBACK
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
