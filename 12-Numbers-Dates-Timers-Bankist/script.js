d6a530cab20dcb48e29508ee40035aad8289a971
Chirag Rangras <chiragrangras012@gmail.com>
2025-10-17 12:04:04 +0530

    modified file

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -305,7 +305,6 @@ const randomInt = (min, max) =>
 // Rounding integers
 console.log(Math.trunc(23.3));
 console.log(Math.round(23.9));
-*/
 
 console.log(5 % 2);
 console.log(5 / 2); // 5 = 2 * 2 + 1
@@ -324,3 +323,29 @@ labelBalance.addEventListener("click", function () {
     if (i % 3 === 0) row.style.backgroundColor = "blue";      
   });
 });
+*/
+
+// Created a date
+/* const now = new Date();
+console.log(now);
+console.log(new Date("Aug 02 2020 18:05:41"));
+console.log(new Date("December 24, 2015"));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+*/
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
